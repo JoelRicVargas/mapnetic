b1a0c8c1c594627f518241fa5977889ff9f7a772
Bee Code <i201614234@ciberte.edu.pe>
2020-04-21 13:29:38 -0500

    Union con rama Perfil

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -15,7 +15,6 @@ export class LoginComponent implements OnInit {
     email : '',
     contra : ''
   }
-
   constructor(private router: Router) { }
 
   ngOnInit(): void {
@@ -29,9 +28,7 @@ export class LoginComponent implements OnInit {
   ingresar(){
     firebase.auth().signInWithEmailAndPassword(this.item.email, this.item.contra)
     .then(function(){
-      
       var user = firebase.auth().currentUser;
-
       if (user != null) {
         user.providerData.forEach(function (profile) {
           console.log("Sign-in provider: " + profile.providerId);
@@ -39,9 +36,7 @@ export class LoginComponent implements OnInit {
           console.log("  Name: " + profile.displayName);
           console.log("  Email: " + profile.email);
           console.log("  Photo URL: " + profile.photoURL);
-
           if(user.emailVerified == false){
-
             user.sendEmailVerification()
             .then(function() {
               console.log('enviando correo');
@@ -69,11 +64,8 @@ export class LoginComponent implements OnInit {
   }
 
   verificarUser(){
-
     var user = firebase.auth().currentUser;
-
     if(user.emailVerified == false){
-
       user.sendEmailVerification().then(function() {
         console.log('enviando correo');
         console.log(user);
@@ -82,7 +74,6 @@ export class LoginComponent implements OnInit {
         // An error happened.
         //console.log(error);
       });
-
     }else{
       console.log('El usuario ya verifico su correo');
     }
@@ -91,7 +82,6 @@ export class LoginComponent implements OnInit {
   observador();
 
   observador() {
-    
     firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
         // User is signed in.
@@ -102,11 +92,9 @@ export class LoginComponent implements OnInit {
         var isAnonymous = user.isAnonymous;
         var uid = user.uid;
         var providerData = user.providerData;
-
         console.log(user.emailVerified);
         // ...
       } else {
-
         console.log('sin usuario');
         // User is signed out.
         // ...
